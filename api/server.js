3de5cc065dd6fcf60f9205676f318395a5403d15
Fernando Velasco <fervelgo@gmail.com>
2021-08-31 22:51:21 +0100

    server halfway done

--- api/server.js
+++ api/server.js
@@ -1,3 +1,56 @@
-// BUILD YOUR SERVER HERE
+const express = require('express');
+const server = express();
+const Dog = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+
+server.use(express.json());
+
+
+server.get('/api/dogs', (req, res) => {
+    Dog.find()
+        .then( dogs => {
+            res.status(200).json(dogs)
+        })
+        .catch( err => {
+            res.status(500).json({ message: err.message})
+        });
+});
+
+server.get('/api/dogs/:id', (req, res) => {
+    const { id } = req.params
+    Dog.findBy(id)
+        .then( dogs => {
+            if (dogs) {
+                res.status(200).json(dogs)    
+            } else {
+            res.status(404).json({message: `Dog with ${id} not found`})
+            }
+        })
+        .catch( err => {
+            res.status(500).json({ message: err.message})
+        });
+});
+
+server.put('api/dogs/:id', (req, res) => {
+    const { id } = req.params
+    const { name, weight } = req.body
+
+    Dog.update(id, {name, weight})
+    .then(updateDog => {
+        if(!updateDog) {
+            res.status(404).json({ message: `Could not find dog ${id}`})
+        } else {
+            res.status(200).json(updateDog)
+        }
+    })
+    .catch( err => {
+        res.status(500).json({message: 'No editing happened here yo'})
+    })
+
+})
+
+
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
